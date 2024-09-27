7f15bc0d2a2aaf397b6a4e5d8fc1c069e81d247e
ZURIÑE BEHOBIDE ZABALEGUI <157796849+zuribehobide23@users.noreply.github.com>
2024-09-27 11:23:28 +0000

    aldaketak

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let datubase = [
+    {
+      "isbn": "1617293563",
+      "egilea": "Raoul-Gabriel Urma",
+      "data": "Nov 15, 2018",
+      "izenburua": "Modern Java in Action: Lambdas, streams, functional and reactive programming",
+      "filename": "8508261-M.jpg"
+    },
+  
+   {
+      "isbn": "9781617291302",
+      "egilea": "Benjamin Muschko",
+      "data": "Mar 09, 2014",
+      "izenburua": "Gradle in Action",
+      "filename": "8514400-M.jpg"
+    },
+  
+       {
+      "isbn": "1883601126",
+      "egilea": "Matt Welsh",
+      "data": "1995",
+      "izenburua": "The Linux bible",
+      "filename": "6764181-M.jpg"
+    },
+    
+  {
+      "isbn": "9781617293290",
+      "egilea": "Dmitry Jemerov",
+      "data": "Feb 19, 2017",
+      "izenburua": "Kotlin in Action",
+      "filename": "8507716-M.jpg"
+    }
+  ]
+
+const BASE = "https://covers.openlibrary.org/b/id/"
+
